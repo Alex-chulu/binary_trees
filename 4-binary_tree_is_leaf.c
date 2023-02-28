7a7db219271ec5c0a959338083176ae06d971d1a
Alex-chulu <chulualexandar@gmail.com>
2023-02-28 15:00:30 +0200

    task 4 fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_is_leaf - function that checks if a node is aleaf
- *
+/**
+ * binary_tree_is_leaf - function that checks if a node is a leaf
  * @node: pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
+ *
+ * Return: 1 if node is a leaf, 0 if not a leaf and 0 if node is NULL
  *
  */
 
